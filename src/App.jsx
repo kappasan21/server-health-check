2c8a1ef186e025f2ac0233f38304414a249dd081
kappasan21 <134040762+kappasan21@users.noreply.github.com>
2025-10-23 14:52:24 +0900

    Update App.jsx
    Bug fix - forgot to call the newly added function to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css'
-import { checkAuthServerStatus, checkDbStatus } from './components/api-requests';
+import { checkAuthServerStatus, checkDbStatus, checkWeatherServerStatus } from './components/api-requests';
 
 
 
